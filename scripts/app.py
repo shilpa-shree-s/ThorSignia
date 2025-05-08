bb40b6916cfbadacf97ee308edd40334743901cd
shilpa-shree <153615594+shilpa-shree-s@users.noreply.github.com>
2025-05-08 15:26:40 +0530

    Create app.py

---
+++ scripts/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__, template_folder='../templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def handler(environ, start_response):
+    return app(environ, start_response)
